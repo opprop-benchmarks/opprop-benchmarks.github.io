3c6af1474c48ce8b7d1f022bb73edd5d694f186a
Luc Edes <60040621+lucedes27@users.noreply.github.com>
2023-05-14 19:02:17 +0200

    Website Updates

--- src/main/java/io/github/eisop/website/EisopSiteGenerator.java
+++ src/main/java/io/github/eisop/website/EisopSiteGenerator.java
@@ -161,6 +161,9 @@ public class EisopSiteGenerator {
                 e.printStackTrace();
             }
 
+            File releaseOrg = new File(String.valueOf(javadocFolder) + "/checker-javadoc/org");
+            FileUtils.copyDirectoryToDirectory(releaseOrg, javadocFolder);
+
             // Move index.html
             //            File releaseHTML = new File(String.valueOf(releaseFolder) +
             // "/docs/tutorial/index.html");
@@ -304,6 +307,11 @@ public class EisopSiteGenerator {
         FileUtils.copyDirectory(latestJavadoc, newJavadoc);
 
         System.out.println("Latest release: " + String.valueOf(latestRelease));
+
+        // Copy CFLogo.png to cf/
+        File cfLogo = new File(String.valueOf(latestRelease) + "/tutorial/CFLogo.png");
+        File newCFLogo = new File(System.getProperty("user.dir") + "/cf/CFLogo.png");
+        FileUtils.copyFile(cfLogo, newCFLogo);
 
         getAFU();
     }
